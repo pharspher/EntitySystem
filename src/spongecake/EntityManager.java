dc3841018eb548c79d38cc5c44a8fbe54eccf2b2
pharspher <pharspher@gmail.com>
2013-07-07 13:51:06 +0800

    1. Add SimpleEntityManager.

--- src/spongecake/EntityManager.java
+++ src/spongecake/EntityManager.java
@@ -1,120 +1,31 @@
 package spongecake;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.lang.IllegalStateException;
-
-public class EntityManager {
-    private static final String TAG = EntityManager.class.getSimpleName();
-
-    private static EntityManager mManager = new EntityManager();
-    private static int mEntityCount = 0;
-
-    //private HashMap<String, ArrayList<Integer>> mComponentEntities;
-    //private HashMap<Integer, ArrayList<Component>> mComponentStores;
-
-    private HashMap<String, HashMap<Integer, ArrayList<Component>>> mComponentStores; 
+public abstract class EntityManager {
+    private static EntityManager mManager;
 
     public static EntityManager getDefaultManager() {
+        mManager = new SimpleEntityManager();
         return mManager;
     }
 
+    public abstract void addComponent(Entity entity, Component component);
+    public abstract void removeComponent(Entity entity, Component component);
+    public abstract void removeComponents(Entity entity, Class<? extends Component> componentClass);
+    public abstract void removeEntity(Entity entity);
+    public abstract void dump();
+
+    protected abstract Entity obtainEntityImpl();
+
     public static Entity obtainEntity() {
-        return obtainEntity("unspecified");
+        return mManager.obtainEntityImpl();
     }
 
-    public static Entity obtainEntity(String tag) {
-        return new Entity(mEntityCount++, tag);
-    }
-
-    public void addComponent(Entity entity, Component component) {
-        String componentName = component.getClass().getSimpleName();
-        int entityID = entity.mID;
-
-        HashMap<Integer, ArrayList<Component>> store = mComponentStores.get(componentName);
-        if (store == null) {
-            store = new HashMap<Integer, ArrayList<Component>>();
-        }
-
-        ArrayList<Component> instanceList = store.get(entityID);
-        if (instanceList == null) {
-            instanceList = new ArrayList<Component>();
-        }
-        instanceList.add(component);
-
-        store.put(entityID, instanceList);
-        mComponentStores.put(componentName, store);
-    }
-
-    public void dump() {
-        Iterator<Map.Entry<String, HashMap<Integer, ArrayList<Component>>>> componentIter = mComponentStores.entrySet().iterator();
-        while (componentIter.hasNext()) {
-            Map.Entry<String, HashMap<Integer, ArrayList<Component>>> pairs = (Map.Entry<String, HashMap<Integer, ArrayList<Component>>>)componentIter.next();
-            Log.d(TAG, (String)pairs.getKey());
-            
-            HashMap<Integer, ArrayList<Component>> store = (HashMap<Integer, ArrayList<Component>>)(pairs.getValue());
-            Iterator<Map.Entry<Integer, ArrayList<Component>>> entityIter = store.entrySet().iterator();
-            while (entityIter.hasNext()) {
-                Map.Entry<Integer, ArrayList<Component>> entity = (Map.Entry<Integer, ArrayList<Component>>)entityIter.next();
-                StringBuilder builder = new StringBuilder();
-                builder.append("\t" + entity.getKey() + ": ");
-                ArrayList<Component> c = (ArrayList<Component>)(entity.getValue());
-                builder.append("[");
-                for (Component cccc : c) {
-                    builder.append(cccc + ", ");
-                }
-                builder.append("]");
-                Log.d(TAG, builder.toString());
-            }
-        }
-    }
-
-    /*
-    public void addComponent(Entity entity, Component component) {
-        String componentName = component.getClass().getSimpleName();
-        int entityID = entity.mID;
-
-        ArrayList<Integer> entities = mComponentEntities.get(componentName);
-        if (entities == null) {
-            ArrayList<Integer> newEntityList = new ArrayList<Integer>();
-            newEntityList.add(entityID);
-            mComponentEntities.put(componentName, newEntityList);
-        } else if (!entities.contains(entityID)) {
-            entities.add(entityID);
-        } else {
-            throw new IllegalStateException("Entity " + entity.mTag + "(" + entityID + ")" + " had already been bound to " + componentName);
-        }
-
-        ArrayList<Component> components = mComponentStores.get(entityID);
-        if (components == null) {
-            ArrayList<Component> newComponentList = new ArrayList<Component>();
-            newComponentList.add(component);
-            mComponentStores.put(entityID, newComponentList);
-        } else {
-            components.add(component);
-        }
-
-        Log.d(TAG, "Bind " + component + " to entity " + entityID);
-    }
-    */
-
-    private EntityManager() {
-        //mComponentEntities = new HashMap<String, ArrayList<Integer>>();
-        //mComponentStores = new HashMap<Integer, ArrayList<Component>>();
-        mComponentStores = new HashMap<String, HashMap<Integer, ArrayList<Component>>>();
-    }
+    protected EntityManager() {}
 
     public static class Entity {
-        private Entity(int id) {
-            this(id, "");
-        }
-        private Entity(int id, String tag) {
+        protected Entity(int id) {
             mID = id;
-            mTag = tag;
         }
-        private int mID;
-        private String mTag;
+        protected int mID;
     }
 }
---
+++ src/spongecake/SimpleEntityManager.java
@@ -0,0 +1,147 @@
+package spongecake;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class SimpleEntityManager extends EntityManager {
+    private static final String TAG = SimpleEntityManager.class.getSimpleName();
+
+    private static int mEntityCount = 0;
+
+    private HashMap<String, HashMap<Integer, ArrayList<Component>>> mComponentStores;
+
+    @Override
+    public Entity obtainEntityImpl() {
+        return new Entity(mEntityCount++);
+    }
+
+    @Override
+    public void addComponent(Entity entity, Component component) {
+        String componentName = component.getClass().getSimpleName();
+        int entityID = entity.mID;
+
+        HashMap<Integer, ArrayList<Component>> componentStore = mComponentStores.get(componentName);
+        if (componentStore == null) {
+            componentStore = new HashMap<Integer, ArrayList<Component>>();
+        }
+
+        ArrayList<Component> componentList = componentStore.get(entityID);
+        if (componentList == null) {
+            componentList = new ArrayList<Component>();
+        }
+
+        componentList.add(component);
+        componentStore.put(entityID, componentList);
+        mComponentStores.put(componentName, componentStore);
+    }
+
+    @Override
+    public void removeComponent(Entity entity, Component component) {
+        String componentName = component.getClass().getSimpleName();
+        int entityID = entity.mID;
+
+        HashMap<Integer, ArrayList<Component>> componentStore = mComponentStores.get(componentName);
+        if (componentStore == null) {
+            throw new IllegalStateException(componentName + " is not component of entity " + entityID);
+        }
+
+        ArrayList<Component> componentList = componentStore.get(entityID);
+        if (componentList == null) {
+            throw new IllegalStateException(componentName + " is not component of entity " + entityID);
+        }
+
+        int componentIndex = componentList.indexOf(component);
+        if (componentIndex == -1) {
+            throw new IllegalStateException(componentName + " is not component of entity " + entityID);
+        }
+
+        componentList.remove(componentIndex);
+        if (componentList.isEmpty()) {
+            componentStore.remove(entityID);
+        }
+
+        if (componentStore.isEmpty()) {
+            mComponentStores.remove(componentName);
+        }
+    }
+
+    @Override
+    public void removeComponents(Entity entity, Class<? extends Component> componentClass) {
+        String componentName = componentClass.getSimpleName();
+        int entityID = entity.mID;
+
+        HashMap<Integer, ArrayList<Component>> componentStore = mComponentStores.get(componentName);
+        if (componentStore == null) {
+            throw new IllegalStateException(componentName + " is not component of entity " + entityID);
+        }
+
+        ArrayList<Component> componentList = componentStore.get(entityID);
+        if (componentList == null) {
+            throw new IllegalStateException(componentName + " is not component of entity " + entityID);
+        }
+
+        componentStore.remove(entityID);
+
+        if (componentStore.isEmpty()) {
+            mComponentStores.remove(componentName);
+        }
+    }
+
+    @Override
+    public void removeEntity(Entity entity) {
+        int entityID = entity.mID;
+
+        /*
+        Iterator<Entry<String, HashMap<Integer, ArrayList<Component>>>> storeIter = mComponentStores.entrySet().iterator();
+        while (storeIter.hasNext()) {
+            Entry<String, HashMap<Integer, ArrayList<Component>>> pairs = (Entry<String, HashMap<Integer, ArrayList<Component>>>)storeIter.next();
+            HashMap<Integer, ArrayList<Component>> store = pairs.getValue();
+            store.remove(entityID);
+            if (store.isEmpty()) {
+                storeIter.remove();
+            }
+        }
+        */
+
+        Set<String> componentSet = mComponentStores.keySet();
+        for (String componentName : componentSet) {
+            HashMap<Integer, ArrayList<Component>> componentStore = mComponentStores.get(componentName);
+            componentStore.remove(entityID);
+            if (componentStore.isEmpty()) {
+                mComponentStores.remove(componentName);
+            }
+        }
+    }
+
+    @Override
+    public void dump() {
+        Log.d(TAG, "==============");
+        Iterator<Map.Entry<String, HashMap<Integer, ArrayList<Component>>>> componentIter = mComponentStores.entrySet().iterator();
+        while (componentIter.hasNext()) {
+            Map.Entry<String, HashMap<Integer, ArrayList<Component>>> pairs = (Map.Entry<String, HashMap<Integer, ArrayList<Component>>>)componentIter.next();
+            Log.d(TAG, (String)pairs.getKey());
+            
+            HashMap<Integer, ArrayList<Component>> store = (HashMap<Integer, ArrayList<Component>>)(pairs.getValue());
+            Iterator<Map.Entry<Integer, ArrayList<Component>>> entityIter = store.entrySet().iterator();
+            while (entityIter.hasNext()) {
+                Map.Entry<Integer, ArrayList<Component>> entity = (Map.Entry<Integer, ArrayList<Component>>)entityIter.next();
+                StringBuilder builder = new StringBuilder();
+                builder.append("\t" + entity.getKey() + ": ");
+                ArrayList<Component> c = (ArrayList<Component>)(entity.getValue());
+                builder.append("[");
+                for (Component cccc : c) {
+                    builder.append(cccc + ", ");
+                }
+                builder.append("]");
+                Log.d(TAG, builder.toString());
+            }
+        }
+    }
+
+    protected SimpleEntityManager() {
+        mComponentStores = new HashMap<String, HashMap<Integer, ArrayList<Component>>>();
+    }
+}
